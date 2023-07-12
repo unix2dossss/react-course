77b9a2fba8c8d66aeda9d9eb928fa7660d1a1b5f
Vandan Bhatt <60042992+unix2dossss@users.noreply.github.com>
2023-07-13 01:29:07 +1200

    Completed Mini Practice Project

--- 02-component-basics/src/App.js
+++ 02-component-basics/src/App.js
@@ -1,43 +1,52 @@
-import keyConceptsImage from './assets/images/key-concepts.png';
-import componentsImage from './assets/images/components.png';
-import stateImage from './assets/images/state.png';
-import eventsImage from './assets/images/events.png';
+import componentsImage from "./assets/images/components.png";
+import stateImage from "./assets/images/state.png";
+import eventsImage from "./assets/images/events.png";
+
+import Concept from "./components/Concept/Concept";
+import Header from "./components/Header/Header";
+import "./index.css";
 
 const concepts = [
   {
-    title: 'Components',
+    title: "Components",
     image: componentsImage,
     description:
-      'Components let you split the UI into independent, reusable pieces, and think about each piece in isolation. Components can receive data via props, and they can render dynamic output using JSX.',
+      "Components let you split the UI into independent, reusable pieces, and think about each piece in isolation. Components can receive data via props, and they can render dynamic output using JSX.",
   },
   {
-    title: 'State',
+    title: "State",
     image: stateImage,
     description:
-      'State is data that may change over time. As it changes, the UI should be updated to reflect the updated data. Each component can maintain its own state and multiple components can share state.',
+      "State is data that may change over time. As it changes, the UI should be updated to reflect the updated data. Each component can maintain its own state and multiple components can share state.",
   },
   {
-    title: 'Events',
+    title: "Events",
     image: eventsImage,
     description:
-      'Event handlers are added via props to (built-in) components. You pass functions as values to such event handlers to control which functions gets executed for which event.',
+      "Event handlers are added via props to (built-in) components. You pass functions as values to such event handlers to control which functions gets executed for which event.",
   },
 ];
 
 function App() {
   return (
     <div>
-      <header>
-        <img src={keyConceptsImage} alt="Medal badge with a star" />
-        <h1>Key React Concepts</h1>
-        <p>Selected key React concepts you should know about</p>
-      </header>
+      <Header></Header>
       <ul id="concepts">
-        <li className="concept">
-          <img src="TODO: IMAGE" alt="TODO: TITLE" />
-          <h2>TODO: TITLE</h2>
-          <p>TODO: DESCRIPTION</p>
-        </li>
+        <Concept
+          image={concepts[0].image}
+          title={concepts[0].title}
+          description={concepts[0].description}
+        ></Concept>
+        <Concept
+          image={concepts[1].image}
+          title={concepts[1].title}
+          description={concepts[1].description}
+        ></Concept>
+        <Concept
+          image={concepts[2].image}
+          title={concepts[2].title}
+          description={concepts[2].description}
+        ></Concept>
       </ul>
     </div>
   );
