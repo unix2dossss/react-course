196641ba299b5da17263eca86f31664cbc930e48
Vandan Bhatt <60042992+unix2dossss@users.noreply.github.com>
2023-07-17 20:02:45 +1200

    Created Form

--- 02-component-basics/src/App.js
+++ 02-component-basics/src/App.js
@@ -1,53 +1,35 @@
-import componentsImage from "./assets/images/components.png";
-import stateImage from "./assets/images/state.png";
-import eventsImage from "./assets/images/events.png";
+import React from 'react';
 
-import Concept from "./components/Concept/Concept";
-import Header from "./components/Header/Header";
-import "./index.css";
+import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
-const concepts = [
-  {
-    title: "Components",
-    image: componentsImage,
-    description:
-      "Components let you split the UI into independent, reusable pieces, and think about each piece in isolation. Components can receive data via props, and they can render dynamic output using JSX.",
-  },
-  {
-    title: "State",
-    image: stateImage,
-    description:
-      "State is data that may change over time. As it changes, the UI should be updated to reflect the updated data. Each component can maintain its own state and multiple components can share state.",
-  },
-  {
-    title: "Events",
-    image: eventsImage,
-    description:
-      "Event handlers are added via props to (built-in) components. You pass functions as values to such event handlers to control which functions gets executed for which event.",
-  },
-];
+const App = () => {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
 
-function App() {
   return (
     <div>
-      <Header></Header>
-      <ul id="concepts">
-        <Concept
-          image={concepts[0].image}
-          title={concepts[0].title}
-          description={concepts[0].description}
-        ></Concept>
-        <Concept
-          image={concepts[1].image}
-          title={concepts[1].title}
-          description={concepts[1].description}
-        ></Concept>
-        <Concept
-          image={concepts[2].image}
-          title={concepts[2].title}
-          description={concepts[2].description}
-        ></Concept>
-      </ul>
+      <NewExpense />
+      <Expenses items={expenses} />
     </div>
   );
 }
